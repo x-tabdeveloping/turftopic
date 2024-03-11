c42210510e9986541f34da0aec04f20f86d55f77
rbroc <rbrrcc@gmail.com>
2024-03-11 14:58:55 +0100

    move encoders batching fn to utils

--- turftopic/encoders/cohere.py
+++ turftopic/encoders/cohere.py
@@ -1,20 +1,10 @@
-import itertools
 import os
-from typing import Iterable, List
+from typing import Iterable
 
 import numpy as np
 
 from turftopic.encoders.base import ExternalEncoder
-
-
-def batched(iterable, n: int) -> Iterable[List[str]]:
-    "Batch data into tuples of length n. The last batch may be shorter."
-    # batched('ABCDEFG', 3) --> ABC DEF G
-    if n < 1:
-        raise ValueError("n must be at least one")
-    it = iter(iterable)
-    while batch := list(itertools.islice(it, n)):
-        yield batch
+from turftopic.encoder.utils import batched
 
 
 class CohereEmbeddings(ExternalEncoder):
