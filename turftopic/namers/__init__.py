2d57c48b65b53c247a705e24159a36791bca2e04
Márton Kardos <power.up1163@gmail.com>
2024-11-04 17:13:34 +0100

    Added N-gram retrieval namer

--- turftopic/namers/__init__.py
+++ turftopic/namers/__init__.py
@@ -1,5 +1,6 @@
 from turftopic.error import NotInstalled
 from turftopic.namers.hf_transformers import LLMTopicNamer
+from turftopic.namers.ngram import NgramTopicNamer
 
 try:
     from turftopic.namers.openai import OpenAITopicNamer
@@ -7,4 +8,4 @@ except ModuleNotFoundError:
     OpenAITopicNamer = NotInstalled("OpenAITopicNamer", "openai")
 
 
-__all__ = ["OpenAITopicNamer", "LLMTopicNamer"]
+__all__ = ["OpenAITopicNamer", "LLMTopicNamer", "NgramTopicNamer"]
