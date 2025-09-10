82ae3dc3cea84384c9bb0b4f927c47ac48410322
Márton KardosØ <power.up1163@gmail.com>
2025-09-10 17:36:05 +0200

    Added Analyzers API and a couple of analyzers

---
+++ turftopic/analyzers/hf_llm.py
@@ -0,0 +1,57 @@
+from typing import Optional
+
+from transformers import pipeline
+
+from turftopic.analyzers.base import Analyzer
+
+
+class LLMAnalyzer(Analyzer):
+    """Analyze topic model with an open LLM from HF Hub.
+
+    Parameters
+    ----------
+    model_name: str, default 'HuggingFaceTB/SmolLM3-3B'
+        Open LLM to use from HF Hub.
+    use_summaries: bool, default False
+        Indicates whether the language model should summarize documents before
+        analyzing the topics.
+    context: str, default None
+        Additional context provided to the analyzer for analysis.
+        e.g. "Analyze topics from blog posts related to morality and religion"
+    system_prompt: str, default None
+        Ignored, exists for compatibility
+    summary_prompt: str, default None
+        Prompt to use for abstractive summarization.
+    namer_prompt: str, default None
+        Prompt template for naming topics.
+    description_prompt: str, default None
+        Prompt template for generating topic descriptions.
+    device: str, default "cpu"
+        ID of the device to run the language model on.
+    """
+
+    def __init__(
+        self,
+        model_name: str = "HuggingFaceTB/SmolLM3-3B",
+        context: Optional[str] = None,
+        use_summaries: bool = False,
+        system_prompt: Optional[str] = None,
+        summary_prompt: Optional[str] = None,
+        namer_prompt: Optional[str] = None,
+        description_prompt: Optional[str] = None,
+        device: str = "cpu",
+    ):
+        self.device = device
+        self.model_name = model_name
+        self.pipeline = pipeline(
+            task="text-generation",
+            model=self.model_name,
+            device=self.device,
+        )
+        self.summary_prompt = summary_prompt or self.summary_prompt
+        self.namer_prompt = namer_prompt or self.namer_prompt
+        self.description_prompt = description_prompt or self.description_prompt
+        self.use_summaries = use_summaries
+
+    def generate_text(self, prompt: str) -> str:
+        return self.pipeline(prompt)
